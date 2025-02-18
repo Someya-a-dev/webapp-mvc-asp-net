95c07e315cef0ada8a1a4e67feb1405124a2e886
keid.someya <someya.ayato.dev@gmail.com>
2025-02-18 15:49:44 +0900

    Update CustomerSearchService.cs

--- WebApp/WebApp/Services/CustomerSearchService.cs
+++ WebApp/WebApp/Services/CustomerSearchService.cs
@@ -31,5 +31,28 @@ namespace WebApp.Services
 
             return _dbContext.Database.SqlQuery<DBCustomerSearchModel>(sql).ToList();
         }
+
+        public List<DBCustomerSearchModel> Getcustomer(DBCustomerSearchModel model)
+        {
+            //順番
+            //SELECT    取得する項目
+            //FROM      取得元のテーブル
+            //WHERE　　 検索条件
+            //ORDER BY  並び替え(ソート)
+            //OFFSET　　何件目から何件目まで設定S
+            string sql = @"
+                SELECT
+                    CUST_ID AS ID,
+                    CUST_NM AS Name,
+                    CUST_TYPE AS Type,
+                    TEL_NO AS PhoneNumber, 
+                    ZIPCODE AS ZipCode, 
+                    ADDRESS AS Address
+                FROM MST_CUSTOMER
+                WHERE 1=1
+            ";
+
+
+        }
     }
 }
